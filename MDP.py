4cdb029ef96613d321b1804f93cace53c79361a8
SAND <112147283+SANDtcs@users.noreply.github.com>
2023-03-21 11:51:38 +0530

    new

--- MDP.py
+++ MDP.py
@@ -17,3 +17,39 @@ for i in range(300):
     expected["hail"]=hail_reward+0.9*sum(hail[x]*prev[x] for x in hail.keys())
    
 print(expected)
+
+
+# ***************************************************************************
+from copy import deepcopy
+
+
+PU={'S':{'PU':1},'A':{'PU':1/2,'PF':1/2}}
+PF={'S':{'PU':1/2,'RF':1/2},'A':{'PF':1}}
+RF={'S':{'RF':1/2,'RU':1/2}, 'A':{'PF':1}}
+RU={'S':{'PU':1/2,'RU':1/2}, 'A':{'PU':1/2,'PF':1/2}}
+
+actions=['S','A']
+
+PU_reward=0
+PF_reward=0
+RF_reward=10
+RU_reward=10
+
+def find_max(action, state, values, prev):
+    sum_vals=[]
+    for i in action:
+        sum_vals.append(sum(values[i][x] * prev[x] for x in values[i].keys()))
+    return max(sum_vals)
+
+expected = {"PU":PU_reward, "PF":PF_reward, "RF":RF_reward, 'RU':RU_reward}
+
+
+for i in range(300):
+    prev=deepcopy(expected)
+   
+    expected["PU"] = PU_reward + 0.9 * find_max(actions, "PU", PU, prev)
+    expected["PF"] = PF_reward + 0.9 * find_max(actions, "PF", PF, prev)
+    expected["RF"] = RF_reward + 0.9 * find_max(actions, "RF", RF, prev)
+    expected["RU"] = RU_reward + 0.9 * find_max(actions, "RU", RU, prev)
+   
+print(expected)
